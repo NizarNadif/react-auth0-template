2c8dc69ac3c6227eb77099503b563b00e4c4d173
Nizar Nadif <56386611+NizarNadif@users.noreply.github.com>
2021-03-26 00:03:28 +0100

    auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
 import "./style.css";
 
 export function App() {
-  return (
-    <div className="app">
-      Hello world!
-    </div>
-  );
+	return (
+		<div className="app">
+			<LoginButton />
+			<LogoutButton />
+			<Profile />
+		</div>
+	);
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import { App } from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const envVariables = process.env;
 
 const rootElement = document.getElementById("app");
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
+	<Auth0Provider
+		domain={process.env.REACT_APP_AUTH_DOMAIN}
+		clientId={process.env.REACT_APP_AUTH_CLIENTID}
+		redirectUri={window.location.origin}
+	>
+		<App />
+	</Auth0Provider>,
+	rootElement
 );
